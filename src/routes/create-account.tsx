7ff1ef8d9ca9b920ea170ee68a6c9a1b36a99e61
DESKTOP-V27LK94\YSY <ysyapr91@gmail.com>
2024-09-25 22:04:37 +0900

    Revert "."
    This reverts commit feb54f5e5e93a748af143dc3535193a9346c2f4d.

--- src/routes/create-account.tsx
+++
@@ -1,7 +0,0 @@
-export default function CreateAccount() {
-    return(
-        <>
-            Create Account
-        </>
-    )
-}
--- src/routes/loading-screen.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingScreen() {
-    return(
-        <>
-            Loading...
-        </>
-    )
-}
