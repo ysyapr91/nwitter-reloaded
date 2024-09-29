45f70fd6c71fc73aaef1d7942953c62f1f21d9f5
DESKTOP-V27LK94\YSY <ysyapr91@gmail.com>
2024-09-29 17:59:34 +0900

    no message

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,15 +1,12 @@
-import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 
 export default function Home() {
-    const navigate = useNavigate();
-
     const logOut = () => {
         auth.signOut();
-      };
-      return (
+    };
+    return (
         <h1>
-          <button onClick={logOut}>Log Out</button>
+            <button onClick={logOut}>Log Out</button>
         </h1>
-      );
-    }
+    );
+}
